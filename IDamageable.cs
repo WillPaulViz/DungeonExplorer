9a537e1fe7f17e6be45d995f1cdcc7d5650c71b4
Will <williamviziteu@gmail.com>
2025-05-01 14:30:09 +0100

    update

--- IDamageable.cs
+++ IDamageable.cs
@@ -3,7 +3,9 @@
     interface IDamageable
     {
         int Health { get; }
-        int MaxHealth { get;  }
+        int MaxHealth { get; }
         int Damage { get; }
+
+        void TakeDMG(int damage);
     }
 }
